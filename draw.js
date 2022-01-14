01e43d7f3d56db617c603cd6e60abe5b31de4ca6
Michael Beebe <mpb9@live.unc.edu>
2022-01-13 19:13:21 -0500

    styling organized

--- draw.js
+++ draw.js
@@ -180,7 +180,7 @@ svg.addEventListener("mousedown", function(e)
 
     if(shapeTime || circleTime){
 
-        ctx.lineWidth = 1;
+        ctx.lineWidth = 2;
         ctx.strokeStyle = 'rgba(0, 0, 0, 1.0)';
 
         if(cleared == 0){
@@ -367,7 +367,7 @@ function fillShape(pts, count, height, width, color, path){
     var x0; 
     var x1;
 
-    for(var y = GRoundToInt(edges[0].top.fY); y <= height; y+= 0.5){
+    for(var y = GRoundToInt(edges[0].top.fY); y <= height; y++){
 
         for(var i = 0; i < size; i++){
             if(GRoundToInt(edges[i].top.fY) <= y && GRoundToInt(edges[i].bottom.fY) > y){
