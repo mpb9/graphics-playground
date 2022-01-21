383e58cc8c8a9fb4a352c55212335c1d76a85a2a
Michael Beebe <mpb9@live.unc.edu>
2022-01-21 02:35:21 -0500

    pawn class good

--- game.js
+++ game.js
@@ -74,55 +74,19 @@ window.addEventListener('load', () => {
             [this.userdiv.style.width, this.userdiv.style.height] = [`0px`, `0px`]
           }
     }
+        
     
     class Game {
-      constructor(x, y, dx, dy, diameter, color, player) {
-        // Initial model
-        Object.assign(this, { x, y, dx, dy, diameter })
+      constructor(pawns, player) {
         
-        // Initial view
-        this.div = document.createElement('div')
-        Object.assign(this.div.style, {
-          left: `${x}px`,
-          top: `${y}px`,
-          width: `${diameter}px`,
-          height: `${diameter}px`,
-          backgroundColor: color,
-          position: 'absolute',
-        })
-        container.appendChild(this.div)
-
-        
+        this.pawns = pawns;
         this.player = player;
 
         requestAnimationFrame(advance)
-
         
       }
       
-      move() {
-        // Update the model
-        let top = container.offsetTop +5;
-        let left = container.offsetLeft +5;
-        [this.x, this.y] = [this.x + this.dx, this.y + this.dy]
-        if (this.x < left 
-            || this.x > container.clientWidth + left -this.diameter ) {
-
-          this.x = Math.max(left, Math.min(this.x, container.clientWidth + left -this.diameter))
-          this.dx = -this.dx
-        } 
-        if (this.y < top 
-            || this.y > container.clientHeight - this.diameter  + top) {
-
-          this.y = Math.max(top, Math.min(this.y, container.clientHeight - this.diameter + top))
-          this.dy = -this.dy
-        } 
-  
-        // Update the view
-        [this.div.style.left, this.div.style.top] = [`${this.x}px`, `${this.y}px`]
-        
-        
-      }
+      
 
       remove() {
         [this.div.style.width, this.div.style.height] = [`0px`, `0px`]
@@ -133,20 +97,23 @@ window.addEventListener('load', () => {
     
     const advance = () => {
       
-        
-        if(game.player.x+(game.player.width) >= game.x 
-            && game.player.x <= game.x + game.diameter
-            && game.player.y <= game.y + game.diameter
-            && game.player.y + game.player.height >= game.y){
+        game.pawns.forEach(pawn => {
+            if(game.player.x+(game.player.width) >= pawn.x 
+            && game.player.x <= pawn.x + pawn.width
+            && game.player.y <= pawn.y + pawn.height
+            && game.player.y + game.player.height >= pawn.y){
             
-            /*if(game.player.width >= game.diameter){
-                game.remove()
+            /*if(game.player.width >= pawn.diameter){
+                pawn.remove()
             } else { */
                 game.player.removePlayer()
             //} 
 
-        } 
-        game.move()
+            }
+
+            pawn.move()
+        });
+        
 
         //game.player.movePlayer()
         
@@ -166,6 +133,16 @@ window.addEventListener('load', () => {
         playerH,
         'rgba(29, 29, 29, 1)'
     );
-    const game = new Game(500, 250, 1, 1, 80, 'rgba(90, 255, 95, 0.6)', player);
+    const pawns = [
+        new Pawn(container.offsetLeft +5, 
+                container.offsetTop +5,
+                3,
+                20, 24),
+        new Pawn(container.offsetLeft + container.clientWidth/2, 
+                container.offsetTop + 5,
+                3,
+                30, 36)
+    ];
+    const game = new Game(pawns, player);
     
 })
--- gameElements/pawn.js
+++ gameElements/pawn.js
@@ -1,53 +1,49 @@
+const containerP = document.querySelector('#gameDisplay');
 
 class Pawn{
-    constructor(x, y){
+    constructor(x, y, ySpeed, width, height){
         // Initial model
         
-        Object.assign(this, { x, y})
+        Object.assign(this, { x, y, ySpeed, width, height})
         
         // Initial view
-        this.pawnDiv = document.createElement('div')
+        this.pawnDiv = document.createElement('div');
+
         Object.assign(this.pawnDiv.style, {
         left: `${x}px`,
         top: `${y}px`,
-        width: `10px`,
-        height: `12px`,
+        width: `${width}px`,
+        height: `${height}px`,
         backgroundColor: `rgba(224, 109, 28, 1)`,
         position: 'absolute',
-        })
-        container.appendChild(this.pawnDiv)
+        });
+
+        this.prevY = this.y;
+
+        containerP.appendChild(this.pawnDiv);
         
         
     }
-    
-      
+
     move() {
-    // Update the model
-    let top = container.offsetTop +5;
-    let left = container.offsetLeft +5;
-    [this.x, this.y] = [this.x + this.dx, this.y + this.dy]
-    if (this.x < left 
-        || this.x > container.clientWidth + left -this.diameter ) {
+        // Update the model
+        let top = containerP.offsetTop +5;
+        this.y =  this.y + this.ySpeed;
+        
+        if (this.y + this.height > containerP.clientHeight + top) {
 
-        this.x = Math.max(left, Math.min(this.x, container.clientWidth + left -this.diameter))
-        this.dx = -this.dx
-    } 
-    if (this.y < top 
-        || this.y > container.clientHeight - this.diameter  + top) {
+          this.y = (this.y - this.prevY)/4 + this.prevY;
+          this.prevY = this.y;
 
-        this.y = Math.max(top, Math.min(this.y, container.clientHeight - this.diameter + top))
-        this.dy = -this.dy
-    } 
-
-    // Update the view
-    [this.pawnDiv.style.left, this.pawnDiv.style.top] = [`${this.x}px`, `${this.y}px`]
+          if(this.prevY >= containerP.clientHeight + top - 160){
+              this.prevY = top + containerP.clientHeight/4;
+          }
+        } 
+  
+        // Update the view
+        this.pawnDiv.style.top =  `${this.y}px`;
+        
+        
+      }
     
-    
-    }
-
-    remove() {
-    [this.pawnDiv.style.width, this.pawnDiv.style.height] = [`0px`, `0px`]
-    }
-
 }
-    
