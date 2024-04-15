5617fa58379014f33598e2402b25ce64fcc9b210
mpb9 <michaelbeebe1031@gmail.com>
2024-04-15 14:34:59 -0400

    initial path width = 10 (not 1)

--- services/draw.js
+++ services/draw.js
@@ -221,9 +221,9 @@ svg.addEventListener("mousedown", function (e) {
   } else {
     let strokeSize = document.querySelector("#sSize");
 
-    if (strokeSize.valueAsNumber === NaN) {
-      ctx.lineWidth = 1;
-      svgLineWidth = 1;
+    if (strokeSize.value === "") {
+      ctx.lineWidth = 10;
+      svgLineWidth = 10;
     } else {
       ctx.lineWidth = Math.min(50, Math.max(1, strokeSize.valueAsNumber / 2));
       svgLineWidth = ctx.lineWidth;
